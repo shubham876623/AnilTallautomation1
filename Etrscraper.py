b88473421e18f61ff0d3a7433f27b752823854ae
shubham876623 <shubhamsaini8766@gmail.com>
2022-04-06 08:59:29 +0530

    included international

--- Etrscraper.py
+++ Etrscraper.py
@@ -1,7 +1,6 @@
 import requests
 import pandas as pd
 from io import BytesIO
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import  Options
 from random import seed
@@ -33,7 +32,7 @@ import re
 import win32con
 import win32gui
 discount_list=["Hindoostan Mills Limited"]                                                                                                                                 
-comany_list=["HINDOOSTAN MILLS LIMITED","DHL EXPRESS IGST","SPAN PUBLICITY","AJANTA TEXTILES","BKS TEXTILES PVT LTD","PETEX","PRYM FASHION ASIA PACIFIC LIMLTD","SHILPA ENTERPRISES","DIPAK TRADING COMPANY","JAIN NARROW FABRICS","THREADS INDIA LTD","ANSUN MULTITECH INDIA LTD","AMFINO TEX INDIA PVT LTD","CASH","YKK INDIA PVT LTD","VARTIKA PACKAGING P LTD","ANTARCTICA LIMITED","DEEYA INTERNATIONAL","KALPATARU PVT LTD","PECKEL","SATTIK PACKAGING PVT LTD","SREE KARPAGAMBAL MILLS LTD"]
+comany_list=["PRYM FASHION ASIA PACIFIC LIMITED","PRYM FASHION GMBH","HINDOOSTAN MILLS LIMITED","DHL EXPRESS IGST","SPAN PUBLICITY","AJANTA TEXTILES","BKS TEXTILES PVT LTD","PETEX","PRYM FASHION ASIA PACIFIC LIMLTD","SHILPA ENTERPRISES","DIPAK TRADING COMPANY","JAIN NARROW FABRICS","THREADS INDIA LTD","ANSUN MULTITECH INDIA LTD","AMFINO TEX INDIA PVT LTD","CASH","YKK INDIA PVT LTD","VARTIKA PACKAGING P LTD","ANTARCTICA LIMITED","DEEYA INTERNATIONAL","KALPATARU PVT LTD","PECKEL","SATTIK PACKAGING PVT LTD","SREE KARPAGAMBAL MILLS LTD"]
 # driver=webdriver.chrome(webdriver_manager)SREE KARPAGAMBAL MILLS LTD
 driver=webdriver.Chrome(ChromeDriverManager().install())
 
@@ -64,8 +63,8 @@ for i  in range (1,len(table)-1,2):
     all_anchor_tag=table[i].find('td').find('a')
     print("https://stockexcel.com/"+all_anchor_tag.get('href'))
     # time.sleep(100)
-    driver.get("https://stockexcel.com/"+all_anchor_tag.get('href'))
-    # driver.get('https://stockexcel.com/BillProcess.aspx?bpd=50151&bk=y')
+    # driver.get("https://stockexcel.com/"+all_anchor_tag.get('href'))
+    driver.get('https://stockexcel.com/BillProcess.aspx?bpd=37103')
     time.sleep(3)
     soup=BeautifulSoup(driver.page_source,'html.parser')
     company_name=soup.find('span',{'id':'ContentPlaceHolder1_lblSupplierName'})
@@ -74,6 +73,7 @@ for i  in range (1,len(table)-1,2):
     invoice_ammount=soup.find('span',{'id':'ContentPlaceHolder1_lblBillAmount'})
     narration=soup.find('span',{'id':'ContentPlaceHolder1_lblSRNO'})
     paid_ammaunt=soup.find('span',{'id':'ContentPlaceHolder1_lblBillPassedAmt_sel'})
+    currency=soup.find('span',{'id':'ContentPlaceHolder1_lblCurrency'})
     
     if company_name.text.upper() in comany_list:
      ###############  opening the windows of tally ##################
@@ -108,143 +108,181 @@ for i  in range (1,len(table)-1,2):
         # if comanin comany_list:
         
         pg.write(invoice_no.text.strip())
-        time.sleep(1)# entering the invoice number ..
-        pg.press('f2')
-        time.sleep(1)
-        today = date.today()  # entering today date ..
-        time.sleep(1)
-        pg.write(today.strftime("%d/%m")) # entering today date .
-        pg.press('enter')
-        pg.press('enter')
-        pg.write(Date.text)  # entering 
-        pg.press('enter')
-        
-        pg.write(company_name.text.strip())#entering comapny name ...... or supplier  name 
-        pg.press("enter", presses=6)
-        time.sleep(1)
-        
-        if company_name.text not in discount_list:
-            pg.write(paid_ammaunt.text.split('.')[1].strip())
-            
-            pg.press("enter", presses=4)
-            time.sleep(1)
-        # print(invoice_ammount.text)
-            pg.write(invoice_ammount.text.strip())
-            time.sleep(1)
-            print(invoice_ammount.text ,paid_ammaunt.text.split('.')[1]+"."+paid_ammaunt.text.split('.')[2])
-            if invoice_ammount.text==paid_ammaunt.text.split('.')[1]+"."+paid_ammaunt.text.split('.')[2]:
-                pg.press("enter", presses=2)
-            else:
-                print(invoice_ammount.text)
-                time.sleep(1)
-                pg.press("enter", presses=3)
-                time.sleep(1)
-                pg.write(invoice_no.text.strip()+"TDS")
-                time.sleep(1)
-                
-                pg.press("enter", presses=5)
-                
-            
-        # 31633
-            table1=soup.find('table',{'id':'ContentPlaceHolder1_gvLedgerBillPros'}).find_all('tr')
-           
-            
-            
-            # time.sleep(100)
-            list_a=[]
-            for i in range(1,len(table1)-1):
-                
-                pg.write(table1[i].find_all('td')[0].text)
-                time.sleep(1)
-                
-                pg.press("enter", presses=1)
-                time.sleep(1)
-                
-                if "-" in table1[i].find_all('td')[2].text.strip():
-                    list_a.append(table1[i].find_all('td')[2].text.strip()[1:])
-                else: 
-                    list_a.append(table1[i].find_all('td')[2].text.strip())
-                    
-                
-                pg.write(list_a[-1])
-                # time.sleep(1)
-                # pg.write("600")
-                time.sleep(1)
-                pg.press("enter", presses=2)
-                time.sleep(1)
-                pg.write(table1[i].find_all('td')[1].text.strip())
-                time.sleep(1)
-                print(table1[i].find_all('td')[0].text)
-                # print(i,len(table1))
-                if i+1==len(table1)-1:
-                    # if "Discount (CD)" in table1[i].find_all('td')[0].text :
-                        # print(i+1,len(table1)-1 ,"andrr")
-                        pg.press("enter", presses=3)
-                else:
-                    pg.press("enter", presses=4)
-                    
-           
-            
-            pg.write("KJL-"+narration.text.strip()) 
+        if "Rs"  in  paid_ammaunt.text.strip():
+            time.sleep(1)# entering the invoice number ..
+            # pg.press('f2')
             # time.sleep(1)
-            pg.press("enter",presses=2)
-           
-            # print("invoice_no",invoice_no.text,"Date",Date.text,"invoice_ammount",invoice_ammount.text,"paid_ammount",paid_ammaunt.text)
-    
-        else:
-            # pass'
-            list_a=[]
-            table1=soup.find('table',{'id':'ContentPlaceHolder1_gvLedgerBillPros'}).find_all('tr')
+            # today = date.today()  # entering today date ..
+            # time.sleep(1)
+            # pg.write(today.strftime("%d/%m")) # entering today date .
+            # pg.press('enter')
+            pg.press('enter')
+            pg.write(Date.text)  # entering 
+            pg.press('enter')
             
-            pg.write(invoice_ammount.text.strip())
+            pg.write(company_name.text.strip())#entering comapny name ...... or supplier  name 
             pg.press("enter", presses=6)
-            for i in range(1,len(table1)-1):
-                if "Discount (CD)"  in table1[i].find_all('td')[0].text:
-                    pg.press("backspace", presses=1)
-                    pg.write("Cr")
-                    pg.press("enter")
-                    
-                pg.write(table1[i].find_all('td')[0].text)
-                time.sleep(1)
+            time.sleep(1)
+            if company_name.text not in discount_list:
+                pg.write(paid_ammaunt.text.split('.')[1].strip())
                 
-                pg.press("enter", presses=1)
+                pg.press("enter", presses=4)
                 time.sleep(1)
-                
-                if "-" in table1[i].find_all('td')[2].text.strip():
-                    list_a.append(table1[i].find_all('td')[2].text.strip()[1:])
-                else: 
-                    list_a.append(table1[i].find_all('td')[2].text.strip())
-                    
-                
-                pg.write(list_a[-1])
-                # time.sleep(1)
-                # pg.write("600")
+            # print(invoice_ammount.text)
+                pg.write(invoice_ammount.text.strip())
                 time.sleep(1)
-                pg.press("enter", presses=2)
-                time.sleep(1)
-                pg.write(table1[i].find_all('td')[1].text.strip())
-                time.sleep(1)
-                # print(table1[i].find_all('td')[0].text)
-                # print(i,len(table1))
-                if i+1==len(table1)-1:
-                    # if "Discount (CD)" in table1[i].find_all('td')[0].text :
-                        print(i+1,len(table1)-1 ,"andrr")
-                        pg.press("enter", presses=3)
+                print(invoice_ammount.text ,paid_ammaunt.text.split('.')[1]+"."+paid_ammaunt.text.split('.')[2])
+                if invoice_ammount.text==paid_ammaunt.text.split('.')[1]+"."+paid_ammaunt.text.split('.')[2]:
+                    pg.press("enter", presses=2)
                 else:
-                    pg.press("enter", presses=4)
+                    print(invoice_ammount.text)
+                    time.sleep(1)
+                    pg.press("enter", presses=3)
+                    time.sleep(1)
+                    pg.write(invoice_no.text.strip()+"TDS")
+                    time.sleep(1)
                     
-           
+                    pg.press("enter", presses=5)
+                    
                 
-            pg.write("KJL-"+narration.text.strip()) 
+            # 31633
+                table1=soup.find('table',{'id':'ContentPlaceHolder1_gvLedgerBillPros'}).find_all('tr')
+               
+                
+                
+                # time.sleep(100)
+                list_a=[]
+                for i in range(1,len(table1)-1):
+                    
+                    pg.write(table1[i].find_all('td')[0].text)
+                    time.sleep(1)
+                    
+                    pg.press("enter", presses=1)
+                    time.sleep(1)
+                    
+                    if "-" in table1[i].find_all('td')[2].text.strip():
+                        list_a.append(table1[i].find_all('td')[2].text.strip()[1:])
+                    else: 
+                        list_a.append(table1[i].find_all('td')[2].text.strip())
+                        
+                    
+                    pg.write(list_a[-1])
+                    # time.sleep(1)
+                    # pg.write("600")
+                    time.sleep(1)
+                    pg.press("enter", presses=2)
+                    time.sleep(1)
+                    pg.write(table1[i].find_all('td')[1].text.strip())
+                    time.sleep(1)
+                    print(table1[i].find_all('td')[0].text)
+                    # print(i,len(table1))
+                    if i+1==len(table1)-1:
+                        
+                            pg.press("enter", presses=3)
+                    else:
+                        pg.press("enter", presses=4)
+                        
+               
+                
+                pg.write("KJL-"+narration.text.strip()) 
+                # time.sleep(1)
+                pg.press("enter",presses=2)
+               
+                # print("invoice_no",invoice_no.text,"Date",Date.text,"invoice_ammount",invoice_ammount.text,"paid_ammount",paid_ammaunt.text)
+        
+            else:
+                # pass'
+                list_a=[]
+                table1=soup.find('table',{'id':'ContentPlaceHolder1_gvLedgerBillPros'}).find_all('tr')
+                
+                pg.write(invoice_ammount.text.strip())
+                pg.press("enter", presses=6)
+                for i in range(1,len(table1)-1):
+                    if "Discount (CD)"  in table1[i].find_all('td')[0].text:
+                        pg.press("backspace", presses=1)
+                        pg.write("Cr")
+                        pg.press("enter")
+                        
+                    pg.write(table1[i].find_all('td')[0].text)
+                    time.sleep(1)
+                    
+                    pg.press("enter", presses=1)
+                    time.sleep(1)
+                    
+                    if "-" in table1[i].find_all('td')[2].text.strip():
+                        list_a.append(table1[i].find_all('td')[2].text.strip()[1:])
+                    else: 
+                        list_a.append(table1[i].find_all('td')[2].text.strip())
+                        
+                    
+                    pg.write(list_a[-1])
+                  
+                    time.sleep(1)
+                    pg.press("enter", presses=2)
+                    time.sleep(1)
+                    pg.write(table1[i].find_all('td')[1].text.strip())
+                    time.sleep(1)
+                    
+                    if i+1==len(table1)-1:
+                        # if "Discount (CD)" in table1[i].find_all('td')[0].text :
+                            print(i+1,len(table1)-1 ,"andrr")
+                            pg.press("enter", presses=3)
+                    else:
+                        pg.press("enter", presses=4)
+                
+                pg.write("KJL-"+narration.text.strip()) 
+                # time.sleep(1)
+                pg.press("enter",presses=2)
+                
+                
+       # it is the casse of if internationally  payment comees ..
+       
+        else:
+            # print("international payment ")
+            time.sleep(1)# entering the invoice number ..
+            # pg.press('f2')
             # time.sleep(1)
-            pg.press("enter",presses=2)
+            # today = date.today()  # entering today date ..
+            # time.sleep(1)
+            # pg.write("31-03") # entering today date .
+            pg.press('enter')
+            # pg.press('enter')
+            pg.write(Date.text)  # entering 
+            pg.press('enter')
+            
+            pg.write(company_name.text.strip())#entering comapny name ...... or supplier  name 
+            pg.press("enter", presses=6)
+            time.sleep(1)
+            pg.write(currency.text+invoice_ammount.text.strip())
+            pg.press("enter", presses=2)
+            exchange_rate=soup.find('span',{'id':'ContentPlaceHolder1_lbBkExchangeRate'})
+            pg.write(exchange_rate.text.strip())
+            pg.press("enter", presses=13)
+            pg.write(currency.text+invoice_ammount.text.strip())
+            pg.press("enter", presses=5)
+           
             
             
-    
+            
+            
+            table1=soup.find('table',{'id':'ContentPlaceHolder1_gvLedgerBillPros'}).find_all('tr')
+               
+                
+                
+            
+            for i in range(1,len(table1)-1):
+                
+           
+                pg.write(table1[i].find_all('td')[1].text.strip())
+           
+
+                pg.press("enter", presses=3)
+            pg.write("KJL-"+narration.text.strip()) 
+             
+            pg.press("enter",presses=2)    
+       
+
     else:
         print("no comapnty name ",company_name.text.upper())
-       
+      
     
-        # time.sleep(5)
-    # time.sleep(100)
-
